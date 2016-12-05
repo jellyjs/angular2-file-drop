298df9061962db2e7ede64545c930758ab9f4ba9
Benedikt Berger <benedikt.berger@live.de>
2016-12-05 21:40:58 +0100

    add .d.ts files for now

---
+++ build/file-drop.d.ts
@@ -0,0 +1,22 @@
+import { EventEmitter, ElementRef } from '@angular/core';
+export interface Options {
+    readAs?: string;
+}
+export declare class FileDropDirective {
+    fileOver: EventEmitter<boolean>;
+    onFileDrop: EventEmitter<File>;
+    options: Options;
+    private element;
+    constructor(element: ElementRef);
+    onDragOver(event: any): void;
+    onDragLeave(event: any): void;
+    onDrop(event: any): void;
+    private readFile(file);
+    private emitFileOver(isOver);
+    private emitFileDrop(file);
+    private pickStrategy();
+    private hasStrategy(type);
+    private getDataTransfer(event);
+    private preventAndStop(event);
+    private haveFiles(types);
+}
---
+++ build/index.d.ts
@@ -0,0 +1,1 @@
+export * from './file-drop-module';
